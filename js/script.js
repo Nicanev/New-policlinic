bf6c3ff0eacb7521ea26142003fd3c5452447eba
Nicanev <nic7ven@gmail.com>
2021-11-29 11:12:49 +0400

    add page "about"

--- js/script.js
+++ js/script.js
@@ -20,8 +20,23 @@ function getValue() {
         result.textContent =value;
         result.style.display = "block";
     }
-    
-    
-
-    
+}
+function getValueCall() {
+    let select = document.getElementById("select");
+    let tel = document.getElementById("tel");
+    let med = document.getElementById("med");
+    let adress = document.getElementById("adress");
+    let result = document.getElementById("result");
+    let block = document.getElementById("block");
+    let num = Math.floor(Math.random() * (9999 - 1000 + 1)) + 1000;
+    let value = "Ваша заявка на вызов врача зарегистрирована!" + "\nНомер заявки: " + num + "\nТелефон: " + tel.value + "\nНомер ОМС: " + med.value + "\nАдрес: " + adress.value;
+    if (select.value=="1") {
+        alert("Выберите специалиста")
+    }
+    else {
+        button.style.display="none";
+        block.style.display ="none";
+        result.textContent =value;
+        result.style.display = "block";
+    }
 }
